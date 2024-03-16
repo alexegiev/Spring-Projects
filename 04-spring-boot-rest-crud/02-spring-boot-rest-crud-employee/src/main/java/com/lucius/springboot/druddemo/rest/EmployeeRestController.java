fef46b5b6661bd3392e07515784a4a4d016fc274
alexegiev <alexegievteo@gmail.com>
2024-03-16 14:05:02 +0200

    Rest Service Supporting CRUD using jpa DAO

--- 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/lucius/springboot/druddemo/rest/EmployeeRestController.java
+++ 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/lucius/springboot/druddemo/rest/EmployeeRestController.java
@@ -2,26 +2,78 @@ package com.lucius.springboot.druddemo.rest;
 
 import java.util.List;
 import com.lucius.springboot.druddemo.entity.Employee;
-import com.lucius.springboot.druddemo.dao.EmployeeDAO;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.lucius.springboot.druddemo.service.EmployeeService;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
 public class EmployeeRestController {
 
-    private EmployeeDAO employeeDAO;
+    private EmployeeService employeeService;
 
-    //quick and dirty solution: inject employee dao
-    public EmployeeRestController(EmployeeDAO theEmployeeDAO) {
-        this.employeeDAO = theEmployeeDAO;
+    public EmployeeRestController(EmployeeService theEmployeeService) {
+        this.employeeService = theEmployeeService;
     }
 
     //expose "/employees" and return list of employees
     @GetMapping("/employees")
     public List<Employee> findAll() {
-        return employeeDAO.findAll();
+        return employeeService.findAll();
     }
 
+    //expose mapping for GET /employees/{employeeId}
+
+    @GetMapping("/employees/{employeeId}")
+    public Employee getEmployeeId(@PathVariable int employeeId) {
+        Employee theEmployee = employeeService.findById(employeeId);
+
+        if(theEmployee == null) {
+            throw new RuntimeException("Employee id not found - " + employeeId);
+        }
+
+        return theEmployee;
+    }
+
+    //expose mapping POST /employees - add new employee
+
+    @PostMapping("/employees")
+    public Employee addEmployee(@RequestBody Employee theEmployee) {
+
+        // also just in case they pass an id in json ... set id to 0
+        //this is to force a save of new item
+
+        theEmployee.setId(0);
+
+        Employee dbEmployee = employeeService.save(theEmployee);
+
+        return dbEmployee;
+    }
+
+    //expose mapping PUT /employees = update existing employee
+
+    @PutMapping("/employees")
+    public Employee updaateEmployee(@RequestBody Employee theEmployee) {
+
+        Employee dbEmployee = employeeService.save(theEmployee);
+
+        return dbEmployee;
+    }
+
+    //expose mapping DELETE /employees/{employeeId} - delete employee
+
+    @DeleteMapping("/employees/{employeeId}")
+    public String deleteEmployee(@PathVariable int employeeId) {
+
+        Employee tempEmployee = employeeService.findById(employeeId);
+
+        // throw exception if null
+
+        if(tempEmployee == null) {
+            throw new RuntimeException("Employee id not found - " + employeeId);
+        }
+
+        employeeService.deleteById(employeeId);
+
+        return "Deleted employee id - " + employeeId;
+    }
 }
--- 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/lucius/springboot/druddemo/service/EmployeeService.java
+++ 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/lucius/springboot/druddemo/service/EmployeeService.java
@@ -1,10 +1,12 @@
-package com.lucius.springboot.druddemo.dao;
+package com.lucius.springboot.druddemo.service;
 
+import com.lucius.springboot.druddemo.dao.EmployeeDAO;
 import com.lucius.springboot.druddemo.entity.Employee;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-public interface EmployeeDAO {
+public interface EmployeeService {
 
     List<Employee> findAll();
 
